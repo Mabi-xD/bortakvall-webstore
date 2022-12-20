346d84a9da6822bf4db8e464379a0db232b461f3
Mabi-xD <marcusbirgersson901123@gmail.com>
2022-12-20 14:59:28 +0100

    Added info btn and that you can click on it to show detail description

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import './style.css'
 
 
 
-let products: any = []
+let products: {} = []
 
 const getProducts = async () => {
   products = await fetchProducts ()
@@ -26,13 +26,44 @@ const renderProducts = () => {
       ${prod.price}kr
       </h3>
       <button class="btn btn-success">Lägg i varukorgen</button>
+      <button class="btn btn-info" data-product-id="${prod.id}">Info</button>
     </div>
   `)
   .join('')
+  // document.querySelector('#product-container')?.classList.remove('hide')
 }
 
-// const renderProducts = () => {
-//   document.querySelector('#product-container')!.innerHTML = products
-// }
+document.querySelector('#product-container')?.addEventListener('click', e => {
+  e.preventDefault()
+  const target = e.target as HTMLElement
+  console.log(e)
+  if(target.textContent === "Info"){
+    console.log(target.id)
+    document.querySelector('#product-container')!.classList.add('hide')
+    const targetNr = Number(target.dataset.productId)
+    console.log(targetNr)
+    const prod = products.data
+    console.log(prod)
+    const findProd = prod.find(product => product.id === targetNr)
+    console.log(findProd)
+    if (findProd){
+      document.querySelector('#info-container')!.innerHTML = `
+        <div class="col-6 col-md-4 col-lg-3">
+          <img class="img-fluid img-thumbnail" src="https://www.bortakvall.se/${findProd.images.large}">
+          <h2>
+          ${findProd.name}
+          <h2>
+          <h3>
+          ${findProd.price}kr
+          </h3>
+          ${findProd.description}
+          <button class="btn btn-success">Lägg i varukorgen</button>
+        </div>
+      `
+    }
+
+  }
+})
+
 
 getProducts()
