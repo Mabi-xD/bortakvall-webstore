f8ed803523ffc34ae938cb930ea57908b60e5ea7
Helga Börjesson <helgaborjesson@gmail.com>
2022-12-28 09:24:03 +0100

    added a modal (pop-up) for order form

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {fetchProducts} from './api'
 import 'bootstrap/dist/css/bootstrap.css'
 import './style.css'
 
-let products: {} = []
+let products: [] = []
 let productsOrder: [] = []
 
 
@@ -14,7 +14,8 @@ let productsOrder: [] = []
 const getProducts = async () => {
   products = await fetchProducts ()
   console.log(products)
-
+  // add quantity to the objects in the array.
+  let prodQuant = products.data.map(prod => (prod.quantity = 0))
   /*
   * Show number of products to the dom
   */
@@ -66,12 +67,19 @@ const addToCart = () => {
       const targetNr = Number(target.dataset.productId)
       const prod = products.data
       const findProd = prod.find(product => product.id === targetNr)
-      productsOrder.push(findProd)
+      const search = productsOrder.find(prod => prod.id === findProd.id)
+      
+      if(search === undefined){
+        productsOrder.push(findProd)
+        findProd.quantity = 1
+      } else {
+        search.quantity += 1
+      }
       console.log('You have added the following product:', productsOrder)
-    }
-    renderToCart()
+      renderToCart()
     getTotal()
-  })
+  }
+})
 }
 
 /*
@@ -155,12 +163,11 @@ document.querySelector('#info-container')?.addEventListener('click', e => {
 
   const renderToCart = () => {
   document.querySelector('#render-cart')!.innerHTML = productsOrder
-    .map(productsOrder => `
-  <div 
+    .map(productsOrder => ` 
   <div class="order-list">
   <p>
-  ${productsOrder.name} </br>
-  ${productsOrder.price}kr
+  ${productsOrder.name} Pris: ${productsOrder.price}kr/st </br>
+  Antal: ${productsOrder.quantity}st = ${productsOrder.price * productsOrder.quantity}kr
   </p>
 </div>
   `)
@@ -171,13 +178,81 @@ document.querySelector('#info-container')?.addEventListener('click', e => {
 ** Displaying the total sum of product order
 */
 const getTotal = () => {
-const total = 0;
-const totalSum = productsOrder.reduce((accumulator,current) => accumulator + current.price, total)
-console.log(totalSum)
+let totalPrice = 0
+productsOrder.forEach(value => {
+  totalPrice += value.price * value.quantity;
+});
+console.log(totalPrice)
 document.querySelector('#total-sum')!.innerHTML = `
-<p>Din totala summa är: ${totalSum} kr</p>
+<p>Din totala summa är: ${totalPrice} kr</p>
 `
 }
+
+
+/*
+** Go to order-form event
+*/
+document.querySelector('#checkout-btn')?.addEventListener('click', e => {
+  e.preventDefault()
+
+  const target = e.target as HTMLElement
+
+  console.log(e)
+  if(target.tagName === "BUTTON"){
+
+    //document.querySelector('#product-container')?.classList.add('hide')
+    //document.querySelector('#cart')?.classList.add('hide')
+    //document.querySelector('#number-of-products')?.classList.add('hide')
+    document.querySelector('#checkout-container')?.classList.remove('hide')
+  }
+  renderSum()
+})
+
+/*
+** Go back from order-form
+*/
+
+document.querySelector('#checkout-container')?.addEventListener('click', e => {
+  e.preventDefault()
+
+  const target = e.target as HTMLElement
+
+  if(target.textContent === "Tillbaka"){
+
+    document.querySelector('#checkout-container')?.classList.add('hide')
+    //document.querySelector('#product-container')?.classList.remove('hide')
+    //document.querySelector('#cart')?.classList.remove('hide')
+  }
+}) 
+
+
+
+const renderSum = () => {
+  document.querySelector('#order-total')!.innerHTML = productsOrder
+    .map(productsOrder => ` 
+  <p>
+  ${productsOrder.name} Pris: ${productsOrder.price}kr/st </br>
+  Antal: ${productsOrder.quantity}st = ${productsOrder.price * productsOrder.quantity}kr
+  </p>
+  `)
+    .join('')
+}
+
+/*
+** Go to order confirmation event
+*/
+
+document.querySelector('#buyBtn')?.addEventListener('click', e => {
+  e.preventDefault()
+
+  const target = e.target as HTMLElement
+
+  console.log(e)
+  if(target.tagName === "BUTTON"){
+    document.querySelector('#checkout-container')?.classList.add('hide')
+    document.querySelector('#confirmation-container')?.classList.remove('hide')
+  }
+})
 
 
 /*
