fa666eb86d21193c7e0bccdd61829a2c051a9e98
Mabi-xD <marcusbirgersson901123@gmail.com>
2023-01-04 17:19:21 +0100

    removed let prodQuant =

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,8 @@ let filterOrder: any
 // GET all products from API
 const getProducts = async () => {
   products = await fetchProducts()
-  let prod = products.data.map((prod: { quantity: number }) => (prod.quantity = 0))
-  let instock = prod.filter((stock: { stock_status: string }) => stock.stock_status === "instock")
+  products.data.map((prod: { quantity: number }) => (prod.quantity = 0))
+  let instock = products.data.filter((stock: { stock_status: string }) => stock.stock_status === "instock")
   document.querySelector('#number-of-products')!.innerHTML = `
   <div class="justify-content-center">
     <p>
@@ -332,7 +332,7 @@ document.getElementById('buyBtn')!.onclick = async () => {
 
   if (orderResponse.data.id !== undefined) {
   document.querySelector('#info-confirmation')!.innerHTML = `
-  <img src="images/202-accepted-order.png" id="acceptedOrderImg" class="image image-fluid" alt="202 accepted cat">
+  <img src="public/202-accepted-order.png" id="acceptedOrderImg" class="image image-fluid" alt="202 accepted cat">
   <br>
   <br>
   <h4>
